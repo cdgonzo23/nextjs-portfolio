932793049da6a79df522617bc249c88abe5e83fb
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-10-11 10:42:37 -0500

    started to build out resume page

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -1,22 +1,24 @@
+"use client";
 import Image from "next/image";
+import { Document, Page, pdfjs } from "react-pdf";
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
 
 export default function Resume() {
   return (
     <div className="hero min-h-screen bg-gradient-to-t from-base-200 from-0% via-primary via-50% to-base-200 to-100%">
-      <div className="flex flex-col items-center py-24">
-        <a
-          className="resume-link pb-12"
-          href="../../../public/assets/Gonzales_Resume.pdf"
-          download
-        >
-          <button className="btn btn-secondary">Download Resume</button>
+      <div className="my-24">
+        <a href="/Gonzales.Resume.pdf" download="Gonzales_Resume" className="link link-hover">
+          Download Resume
         </a>
-        <Image
-          src="/Gonzales_Resume.png"
-          alt="resume"
-          width="700"
-          height="700"
-        ></Image>
+        <Document className="p-3 bg-neutral" file="/Gonzales_Resume.pdf">
+          <Page pageNumber={1} />
+        </Document>
       </div>
     </div>
   );
